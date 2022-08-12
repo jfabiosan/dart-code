86088373f027d4821e44963bef50ad263329121e
josefabiosan <josefabiosan@gmail.com>
2022-08-11 23:59:50 -0300

    aula basica de dart

---
+++ print.dart
@@ -0,0 +1,9 @@
+void main() {
+  //variaveis
+  String nomeLoja = 'Loja Crismart';
+  String endereco = 'Pista de Boquim';
+  int numero = 32;
+
+  print('Hello World');
+  print('O endereco da $nomeLoja fica na $endereco numero $numero');
+}
