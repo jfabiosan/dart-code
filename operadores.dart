b619e60fee337d7f741eccedc5906c7b70bbaad7
josefabiosan <josefabiosan@gmail.com>
2022-09-05 14:51:15 -0300

    estudando operadores na DIO

---
+++ operadores.dart
@@ -0,0 +1,18 @@
+void main() {
+  //operacoes aritmeticas
+  int v1 = 2;
+  double v2 = 30.8;
+  int v3 = 7;
+
+  double result1 = v1 + v2;
+  double result2 = v1 * v2;
+  int result3 = v2 ~/ v1;
+  double result4 = v3 / v1;
+
+  print(result1);
+  print(result2);
+  print(result3);
+  print(result4);
+
+  result1 = v1 % v2;
+}
