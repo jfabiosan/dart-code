3e058e30d8fee671825c9cc36b474f6c91413601
josefabiosan <josefabiosan@gmail.com>
2022-11-29 20:04:05 -0300

    Fazendo teste com varuavel tipo const

--- test-dart.dart
+++ test-dart.dart
@@ -2,8 +2,12 @@ void main () {
 //tres tipos de variaveis
 // DART - var, final e const
 // JS 	- VAR, let e const 
-	
-	final a = 240;
-	print (a);
+final a = 240;
+const nome = "Constante";
+    print (a);
+    print (nome);
+nome = "Variavel"; //Vai gerar erro porque a variavel (nome)é do tipo constante e naonpode ser alterado depois de inicializado.
+    print (nome);
+
 
 }
