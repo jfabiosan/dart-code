dbf8718c7654a3ba901ced5f1714de497202c4a6
Fabio <josefabiosan@gmail.com>
2023-06-16 00:06:00 -0300

    treinando

--- while.dart
+++ while.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 main() {
   bool condicao = true;
   while (condicao) {
-    print("###escreva umn texto###");
+    print("###escreva um texto###");
     String text = stdin.readLineSync()!;
     if (text == "sair") {
       condicao = false;
