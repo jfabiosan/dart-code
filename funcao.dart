8d55880f68f144e942fef93c267dddc1c4fa9af4
Fabio <josefabiosan@gmail.com>
2023-06-17 23:53:59 -0300

    inserindo dados

--- funcao.dart
+++ funcao.dart
@@ -1,8 +1,12 @@
+import 'dart:io';
+
 void main() {
   print(hello());
   hello2();
   param("fabio");
   print(saudacao('José'));
+  print(soma(5, 10));
+  print("A soma dos valores digitados são: ${calculo(" ", " ")}");
 }
 
 String hello() {
@@ -19,4 +23,18 @@ void param(name) {
 
 String saudacao(name) {
   return "Ola $name, seja muito bem vindo novamente!";
+}
+
+int soma(a, b) {
+  return a + b;
+}
+
+int calculo(a, b) {
+  print('Digite um numero');
+  var line = stdin.readLineSync()!;
+  var number1 = int.parse(line);
+  print('Digite outro numero para somar');
+  line = stdin.readLineSync()!;
+  var number2 = int.parse(line);
+  return number1 + number2;
 }
